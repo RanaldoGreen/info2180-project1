b1251e7beb5a79fc38b12d210df90f1bfce4009f
Ranaldo Green <denogreen61@gmail.com>
2022-10-30 14:51:17 -0500

    Added initial starter files

---
+++ scripts.js
@@ -0,0 +1,8 @@
+/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded',function(){
+
+    function myFunction() {
+        document.getElementsByClassName("message").innerHTML = "Hello World";
+    }
+
+})    
