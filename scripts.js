7bed4646833557e66ca39f89d89291c56b9e4037
Ranaldo Green <denogreen61@gmail.com>
2022-10-30 21:58:07 -0500

    Added initial starter files

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
-/* Add your JavaScript to this file */
 document.addEventListener('DOMContentLoaded',function(){
-
-    function myFunction() {
-        document.getElementsByClassName("message").innerHTML = "Hello World";
-    }
-
-})    
+    var  btn = document.getElementsByClassName("btn");
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    btn.item(1).addEventListener('click', function(f){
+        f.preventDefault(); 
+        var email = document.getElementById('email').value;
+        if(email.length==0 || !email.match(pattern)){
+            alert('Please enter a valid email address.');
+            return;
+        }
+        if(email.match(pattern)){
+            msg = document.getElementsByClassName("message")[0];
+            msg.textContent = 'Thank You! Your email address ' + email + ' has been added to our mailing list!';
+        }
+    });
+})
